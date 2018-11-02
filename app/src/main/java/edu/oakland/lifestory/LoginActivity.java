957d2d1199df604681b817fad249f87c75a95f98
Pankaj <mepankaj@live.in>
2018-11-02 10:25:42 -0400

    few more changes to login NOT complete yet

---
+++ app/src/main/java/edu/oakland/lifestory/LoginActivity.java
@@ -0,0 +1,23 @@
+package edu.oakland.lifestory;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        // mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+    }
+}

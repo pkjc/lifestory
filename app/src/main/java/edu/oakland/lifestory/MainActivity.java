cd4db36a93eff0c20e03a01653bf016e4806cb6c
Rashmi Pethe <rashmi.s.kadam@gmail.com>
2018-11-07 12:58:23 -0500

    Added navigation and memory list to home

--- app/src/main/java/edu/oakland/lifestory/MainActivity.java
+++ app/src/main/java/edu/oakland/lifestory/MainActivity.java
@@ -1,13 +1,27 @@
 package edu.oakland.lifestory;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AppHomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/edu/oakland/lifestory/Memory.java
@@ -0,0 +1,28 @@
+package edu.oakland.lifestory;
+
+public class Memory {
+
+    private String memoryTitle;
+    private String memoryText;
+
+    public String getMemoryTitle() {
+        return memoryTitle;
+    }
+
+    public void setMemoryTitle(String memoryTitle) {
+        this.memoryTitle = memoryTitle;
+    }
+
+    public String getMemoryText() {
+        return memoryText;
+    }
+
+    public void setMemoryText(String memoryText) {
+        this.memoryText = memoryText;
+    }
+
+    public Memory(String memoryTitle, String memoryText) {
+        this.memoryTitle = memoryTitle;
+        this.memoryText = memoryText;
+    }
+}

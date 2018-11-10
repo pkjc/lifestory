fc0c8674c819e39f4fd6b4cc47a57879f0cfbe63
Pankaj <mepankaj@live.in>
2018-11-10 13:39:05 +0530

    login completed. still being tested

---
+++ app/src/main/java/edu/oakland/lifestory/utils/Constants.java
@@ -0,0 +1,9 @@
+package edu.oakland.lifestory.utils;
+
+public class Constants {
+
+    public static final String FIREBASE_URL = "https://lifestory-e9bc3.firebaseio.com";
+    public static final String FIREBASE_LOCATION_USERS = "users";
+    public static final String FIREBASE_URL_USERS = FIREBASE_URL + "/" + FIREBASE_LOCATION_USERS;
+    public static final String FIREBASE_PROPERTY_TIMESTAMP = "timestamp";
+}
